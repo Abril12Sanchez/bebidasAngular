9cf6c13fae9934cf29c947697d03341a3f37ffa7
Daniel18UvH <ulisesvh.ti22@utsjr.edu.mx>
2025-07-07 15:39:54 -0600

    npm install @angular/router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http'; // <- Importación correcta
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes),
-     provideHttpClient(withFetch())
-    ]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()) // <- Correcto si estás usando Fetch API
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { EditarBebidaComponent } from './pages/editar-bebida/editar-bebida.component';
 
 export const routes: Routes = [
-    {
-        path: 'editar-empleado/:id',
-        component: EditarBebidaComponent
-    },
+  {
+    path: 'editar-empleado/:id',
+    component: EditarBebidaComponent
+  }
 ];
